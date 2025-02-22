37392963016be225ffd22c52391a4ac1c6bc985c
Ayushmaurya <ayushmaurya3596@gmail.com>
2025-02-22 19:28:38 +0530

    Initial commit

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://ayushmaurya3596:Ayush21@cluster0.gnbc7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected successfully");
   } catch (err) {
     console.error("MongoDB connection failed:", err.message);
